18f8fd13af4d166a0a9e8a3901454c2ccdb4271c
ritchie <johnritchiecoscos52@gmail.com>
2021-09-20 19:14:30 +0800

    first commit

---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Demo | Profile</title>
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Profile Page</h1>
+    Home | <a href="{{url('/')}}">Home</a> | Profile
+</body>
+</html>
